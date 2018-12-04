ca084093c7ce6cc49b15724d551130e437d97f7e
s17007 <s17007@std.it-college.ac.jp>
2018-12-04 14:23:12 +0900

    ジャンル選択追加

--- app/src/main/java/jp/ac/it_college/std/s17007/quiz/NormalActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s17007/quiz/NormalActivity.kt
@@ -2,11 +2,27 @@ package jp.ac.it_college.std.s17007.quiz
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.select_genre.*
 
 class NormalActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.select_genre)
+        val genre = arrayOf(
+            "闇鍋問題",
+            "スポーツ",
+            "東方project",
+            "ポケモン",
+            "サイゲアプリ",
+            "ロボ",
+            "タスゴン、おバイクしよう",
+            "れいやと音楽",
+            "シンデレラガールズ",
+            "TWICE",
+            "まいんくらふと")
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,genre)
+        genre_list.adapter = adapter
 
     }
 }
